6d475aba0c6a541c82db79fb6bb525aa3b020cb5
Gagan <acumind@gmail.com>
2013-07-08 11:49:29 +0000

    Added index.html

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var welcomeString = fs.readFileSync("index.html",'r');
 app.get('/', function(request, response) {
-  response.send('<<<*>>> Welcome To Avair <<<*>>>');
+  response.send('<<<*>>> Welcome To Avair <<<*>>>' + "   " + welcomeString);
 });
 
 var port = process.env.PORT || 5000;
