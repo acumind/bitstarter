4b910a34470f48ce4e1c6abdd4fb8ddf1adcc302
Gagan <acumind@gmail.com>
2013-07-08 16:10:04 +0000

    debug

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 
 var welcomeString = fs.readFileSync("index.html",'utf8');
 app.get('/', function(request, response) {
-  response.send(welcomeString);
+  response.send("Hello World");//welcomeString);
 });
 
 var port = process.env.PORT || 5000;
